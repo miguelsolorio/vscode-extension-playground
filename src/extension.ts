4d5e2ed9c7e471910b1ba12c473c6211b4b18e8b
Miguel Solorio <miguel.solorio@microsoft.com>
2021-02-23 08:51:54 -0800

    add status bar item

--- src/extension.ts
+++ src/extension.ts
@@ -6,9 +6,18 @@ let myStatusBarItem: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
 
+	// status bar item
+	const myCommandId = 'extension.helloWorld';
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
+	myStatusBarItem.command = myCommandId;
+	myStatusBarItem.text = `$(fluent-logo) Hello World`;
+	myStatusBarItem.show();
+	context.subscriptions.push(myStatusBarItem);
+
 	// toast
 	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
 		vscode.window.showInformationMessage(`👋 Hello World!`);
 	});
 	context.subscriptions.push(disposable);
+
 }
